d77c84b8b622d98efff9307bf4004034e8fcb313
lopeselio <lopeselio@gmail.com>
2021-04-06 01:35:43 +0530

    added front end inventory and chainlink VRF

--- blockchain/src/components/App.js
+++ blockchain/src/components/App.js
@@ -1,50 +1,134 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+// import logo from '../logo.png';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRewarded: false,
+      isMinting: false,
+      isLoggedIn: false,
+    };
+  }
+  
+
   render() {
+    const isLoggedIn = this.state.isLoggedIn
+    const isMinting = this.state.isMinting
+
+    const isRewarded = this.state.isRewarded
     return (
-      <div>
+      <div className="screen">
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Dungeons Game
           </a>
         </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
+        <div className='nft-sec'>
+          <div className="dungeon">
+            <div className="avatar-section">
+              <Container>
+                <Row>
+                  <Col sm={8}>
+                    <div>
+                      {avatar(isLoggedIn)}
+                    </div>
+                  </Col>
+                  <Col sm={4}>
+                    <div>
+                      {mint(isMinting)}
+                    </div>
+                  </Col>
+
+                </Row>
+              </Container>
+            </div>
+            
+            <div className='trophy-cabinet'>
+              <div className="title">
+                <span className="text">Your Trophy Cabinet</span>
+              </div>  
+              <div className="scroll">           
+                <div class="container vertical-scrollable">
+                  <div class="row text-center">
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+                      <div>{box(isRewarded)}</div>
+
+                  </div>
+                </div>
+              </div> 
+            </div>
           </div>
-        </div>
+        </div>        
       </div>
     );
+  }
+}
+
+function box(props) {
+  const isRewarded = props.isRewarded;
+  if (isRewarded) {
+    return (
+      <div className='drawer col-xs-4'></div>
+    )
+
+  }else {
+  return (
+    <div className='drawer col-xs-4'>NFT</div>
+  );
+  }
+}
+
+function avatar(props) {
+  const isLoggedIn= props.isLoggedIn;
+  if (isLoggedIn) {
+    return (
+      <div className='avatar'></div>
+    )
+
+  }else {
+  return (
+    <div className='avatar'>AVATAR</div>
+  );
+  }
+}
+
+function mint(props) {
+  const isMinting= props.isMinting;
+  if (isMinting) {
+    return (
+      <div className='minted'></div>
+    )
+
+  }else {
+  return (
+    <div className='minted'>YOUR NEW NFT</div>
+  );
   }
 }
 
--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,22 +1,51 @@
-require('babel-register');
-require('babel-polyfill');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const Web3 = require("web3");
+// require('babel-polyfill');
+// require('babel-register');
+
+const mnemonic = process.env.MNEMONIC
+const Url = process.env.RPC_URL
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+    cldev: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
     },
+    ganache: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
+
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, Url)
+      },
+    network_id: "4",
+    networkCheckTimeout: 1000000,
+    timeoutBlocks: 200,
+    skipDryRun: false
+    // addressIndex: 2
+    
+
+  }
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version: "0.6.12",
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 1
       }
     }
-  }
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
 }
