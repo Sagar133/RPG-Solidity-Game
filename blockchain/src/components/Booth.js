145fb120d7b4b0b66d4dd964579a779de19f441b
Elvin-Skaria <encorereloded@gmail.com>
2021-04-08 02:08:40 +0530

    All changes

---
+++ blockchain/src/components/Booth.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/booth.css'
+
+
+export default function App() {
+  return (
+    <div className="main">
+      <div className="header">
+          <h1>Welcome to the Hero Voting Page</h1>
+      </div>
+      <div className="selection">
+        
+      </div>
+    </div>
+  )
+}
