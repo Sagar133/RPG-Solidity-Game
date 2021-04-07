3f1d2a6b3c89a9889613b6d0f6652903381377ed
lopeselio <lopeselio@gmail.com>
2021-04-08 02:35:39 +0530

    added script to generate characters

--- blockchain/scripts/generate-character.js
+++ blockchain/scripts/generate-character.js
@@ -3,9 +3,11 @@ const TrophyChar = artifacts.require('TrophyChar')
 module.exports = async callback => {
   const trp = await TrophyChar.deployed()
   console.log('Creating requests on contract:', trp.address)
-  const tx = await trp.requestNewRandomTrophy(77, "The Chainlink Knight")
-  const tx2 = await trp.requestNewRandomTrophy(7777777, "The Chainlink Elf")
-  const tx3 = await trp.requestNewRandomTrophy(7, "The Chainlink Wizard")
-  const tx4 = await trp.requestNewRandomTrophy(777, "The Chainlink Orc")
+  const tx = await trp.requestNewRandomTrophy(77, "btc")
+  const tx2 = await trp.requestNewRandomTrophy(7777777, "crystal")
+  const tx3 = await trp.requestNewRandomTrophy(7, "god")
+  const tx4 = await trp.requestNewRandomTrophy(777, "legend")
+  const tx5 = await trp.requestNewRandomTrophy(7777, "rarible")
+
   callback(tx.tx)
 }
--- blockchain/scripts/set-token-uri.js
+++ blockchain/scripts/set-token-uri.js
@@ -3,10 +3,12 @@ const TOKENID = 0
 module.exports = async callback => {
     const trp = await TrophyChar.deployed()
     console.log('Let\'s set the tokenURI of your characters')
-    const tx = await trp.setTokenURI(0, "https://ipfs.io/ipfs/QmaSED9ZSbdGts5UZqueFJjrJ4oHH3GnmGJdSDrkzpYqRS?filename=the-chainlink-knight.json")
-    const tx1 = await trp.setTokenURI(1, "https://ipfs.io/ipfs/QmTvsVaaHTuMNmwXgbfgkrztFEazAPyzmrb4VSS2PbqLjA?filename=the-chainlink-elf.json")
-    const tx2 = await trp.setTokenURI(2, "https://ipfs.io/ipfs/QmPZQhiBB6pwcxRgwZe2mx6ZizCPYgq8i4FBMETyWK1V2z?filename=the-chainlink-wizard.json")
-    const tx3 = await trp.setTokenURI(3, "https://ipfs.io/ipfs/QmS6aznzxshLMcECPQZjCR94UF22WHu6FMM5HLQvaYL9NP?filename=the-chainlink-orc.json")
+    const tx = await trp.setTokenURI(0, "https://ipfs.io/ipfs/QmZGmdAdCHfpBY1YWGvDbf7FHfp48qW1e6GHjWDEYT1eHH?filename=legend.json")
+    const tx1 = await trp.setTokenURI(1, "https://ipfs.io/ipfs/QmW7k5QVeCqbgXqdxakBhdv2msUD5fHgyrf5ecHMQy65yX?filename=god.json")
+    const tx2 = await trp.setTokenURI(2, "https://ipfs.io/ipfs/QmUNweQ468ddeHr97M7zXTLVcAxNH6avzia2Mt5ZjS5fqb?filename=rarible.json")
+    const tx3 = await trp.setTokenURI(3, "https://ipfs.io/ipfs/QmRMEXZMxtpYhW4GU5P2ziRyZDj3LcyTWxYNUZd111XE31?filename=btc.json")
+    const tx4 = await trp.setTokenURI(4, "https://ipfs.io/ipfs/Qme9qTYNQAH4BQd6g1C5ySdQi2HEwoW8KUpd1oCzGinzye?filename=crystal.json")
+    
     console.log(tx)
     callback(tx.tx)
 }
