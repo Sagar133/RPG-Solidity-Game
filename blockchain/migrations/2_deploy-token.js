95d41d9cb41af8d176628b27c8417222be48ccd3
Sagar Behara <sagar@cryptocontrol.io>
2021-04-11 21:51:41 +0530

    minor changes

--- blockchain/migrations/2_deploy-token.js
+++ blockchain/migrations/2_deploy-token.js
@@ -1,9 +1,9 @@
 const DungenCoin = artifacts.require("DungenCoin");
-const Controller = artifacts.require("Controller");
+const Controller = artifacts.require("GameController");
 
 module.exports = async function (deployer) {
-    deployer.deploy(DungenCoin);
+    await deployer.deploy(DungenCoin);
     const token = await DungenCoin.deployed();
 
-    deployer.deploy(Controller, token.address, '0xd8bD0a1cB028a31AA859A21A3758685a95dE4623');
+    await deployer.deploy(Controller, token.address, '0xd8bD0a1cB028a31AA859A21A3758685a95dE4623');
 };
