c9f70de5e1d618a7248fc08b523293bb129ee8bf
lopeselio <lopeselio@gmail.com>
2021-04-11 16:05:30 +0530

    props

--- blockchain/src/components/Box.js
+++ blockchain/src/components/Box.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Image, Button } from 'react-bootstrap'
-import dummy from '../assets/dummy.jpeg'
 import { BsHeart } from "react-icons/bs";
 import {FiThumbsUp} from 'react-icons/fi'
 
-export default function Box() {
+export default function Box(props) {
     return (
           <div>
             <div className="imageBox">
-            <Image src={dummy} roundedCircle style={{height: 150, width: 150, alignSelf:'center', justifyContent:'center'}}/>
+            <Image src={props.img} roundedCircle style={{height: 150, width: 150, alignSelf:'center', justifyContent:'center'}}/>
             </div>
             <div className="imageDetails">
-              <p> Submitted by: The Artist </p>
+              <p className="text-header1"> Submitted by: <span className="name-artist">{props.name}</span> </p>
                 <BsHeart />
                 <div className="d-flex flex-row-reverse">
                 <Button variant="primary">Vote <FiThumbsUp /></Button>
