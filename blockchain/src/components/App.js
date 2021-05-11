6a354923d9a6494c3c5c7eae711ef9c841ffb7b1
lopeselio <lopeselio@gmail.com>
2021-05-11 17:13:39 +0530

    App

--- blockchain/src/components/App.js
+++ blockchain/src/components/App.js
@@ -102,6 +102,7 @@ class App extends Component {
             </li>
           </ul>
         </nav>
+        
         <div className='nft-sec container-fluid mt-5'>
           <div className='row'>
             <main role='main' className='col-lg-12 d-flex text-center'>
