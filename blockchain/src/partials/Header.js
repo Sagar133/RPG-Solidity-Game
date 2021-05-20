63cc10ded04852a35b4a9b41af7e58346957accf
lopeselio <lopeselio@gmail.com>
2021-05-20 16:33:49 +0530

    added async login

--- blockchain/src/partials/Header.js
+++ blockchain/src/partials/Header.js
@@ -1,22 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import {Navbar, Nav, NavDropdown, Form,  FormControl, Button } from 'react-bootstrap'
+import React, { Component } from 'react';
+import {Navbar, Nav, Button } from 'react-bootstrap'
 import './css/Header.css'
+import Web3 from 'web3';
 import { Link } from 'react-router-dom';
 import { scroller } from 'react-scroll';
+import { Renderer } from 'phaser';
+import TropyChar from '../abis/TrophyChar.json'
+import DungeonToken from '../abis/DungeonToken.json'
 
-function Header() {
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      account: '',
+      contract: null,
+      usersNftCount: 0,
+      trophies: [],
+    };
+    this.loadWeb3();
+    this.loadBlockchainData();
+  }
 
-//   const [top, setTop] = useState(true);
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert("Non ethereum browser detected. You should consider trying Metamask")
+    }
+  }
 
-//   // detect whether user has scrolled the page down by 10px 
-//   useEffect(() => {
-//     const scrollHandler = () => {
-//       window.pageYOffset > 10 ? setTop(false) : setTop(true)
-//     };
-//     window.addEventListener('scroll', scrollHandler);
-//     return () => window.removeEventListener('scroll', scrollHandler);
-//   }, [top]);  
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
 
+    const networkId = await web3.eth.net.getId()
+    const networkData = TropyChar.networks[networkId]
+    if (networkData) {
+      const abi = TropyChar.abi;
+      const address = networkData.address
+      const contract = new web3.eth.Contract(abi, address)
+      const contract2 = new web3.eth.Contract(DungeonToken.abi, DungeonToken.networks[networkId].address)
+      //console.log(this.state.account)
+      this.setState({ contract })
+      this.setState({ contract2 })
+
+
+      const usersNftCount = await contract.methods.usersNftCount(this.state.account).call()
+
+      this.setState({ usersNftCount: Number(usersNftCount.toString()) })
+
+      console.log(usersNftCount.toString());
+
+      for (var i = 1; i <= this.state.usersNftCount; i++) {
+        const trophy = await contract.methods.usersNft(this.state.account, i).call()
+        //console.log(trophy);
+        this.setState({
+          trophies: [...this.state.trophies, trophy]
+        })
+      }
+
+      //console.log('users trophies', this.state.usersNftCount, this.state.trophies)
+    } else {
+      window.alert('Smart contract not deployed to the detected network')
+    }
+  }
+
+  render(){
   return (
     <>
         <Navbar className="mov" bg="dark" expand="lg" sticky="top">
@@ -24,10 +79,12 @@ function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto" inline>
-              <Nav.Link onClick={() => scroller.scrollTo('hero', {
-                smooth: true,
-                offset: -70,
-                duration: 1000 })} 
+              <Nav.Link 
+              onClick={() => 
+                scroller.scrollTo('hero', {
+                  smooth: true,
+                  offset: -70,
+                  duration: 1000 })} 
                 className="link" href="#hero">Welcome</Nav.Link>
               <Nav.Link onClick={() => scroller.scrollTo('descr', {
                 smooth: true,
@@ -38,13 +95,14 @@ function Header() {
               <Nav.Link className="link" href="#link">Features</Nav.Link>
               <Nav.Link className="link" href="#link">Contact Us</Nav.Link>
               <Link to="/selection">
-                <Button variant="outline-primary">Login using Metamask</Button>
+                <Button variant="outline-primary">{this.state.account}</Button>
               </Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
     </>
   );
+}
 }
 
 export default Header;
