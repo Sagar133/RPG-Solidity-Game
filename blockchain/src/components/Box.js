2b4368c8f53c18f510df394d138966317776d8fd
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-05-28 08:28:09 +0530

    integrated hero section with the backend, initial prototype

--- blockchain/src/components/Box.js
+++ blockchain/src/components/Box.js
@@ -4,6 +4,33 @@ import { BsHeart } from "react-icons/bs";
 import {FiThumbsUp} from 'react-icons/fi';
 
 export default function Box(props) {
+  // function handleUpvote(){
+  //       const requestOptions = {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify({ title: "React POST Request Example" }),
+  //       };
+  //       fetch("https://reqres.in/invalid-url", requestOptions)
+  //         .then(async (response) => {
+  //           const isJson = response.headers
+  //             .get("content-type")
+  //             ?.includes("application/json");
+  //           const data = isJson && (await response.json());
+
+  //           // check for error response
+  //           if (!response.ok) {
+  //             // get error message from body or default to response status
+  //             const error = (data && data.message) || response.status;
+  //             return Promise.reject(error);
+  //           }
+
+  //           this.setState({ postId: data.id });
+  //         })
+  //         .catch((error) => {
+  //           this.setState({ errorMessage: error.toString() });
+  //           console.error("There was an error!", error);
+  //         });
+  // }
     return (
       <div className="action-cards">
         <div className="card-owner-like">
